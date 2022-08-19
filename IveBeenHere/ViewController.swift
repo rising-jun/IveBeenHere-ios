1476621c31c57c25f053bb24805387be39e03bbe
rising-jun <jun36817@naver.com>
2022-08-19 16:39:28 +0900

    Initial Commit

---
+++ IveBeenHere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IveBeenHere
+//
+//  Created by 김동준 on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
